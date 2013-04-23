faa8739dda1bb3ffbf609aa9cc9aefb735381f7b
chrishenry <henry.christopher@gmail.com>
2013-04-23 19:39:39 -0400

    Start work on array formatter

--- Vanity.py
+++ Vanity.py
@@ -18,14 +18,75 @@ class VanityPhpCommand(sublime_plugin.TextCommand):
             self.control_statements(edit)
         elif action == 'single_line_curlies':
             self.single_line_curlies(edit)
+        elif action == 'array_format':
+            self.array_format(edit)
         else:
             print "Vanity doesn't know what to do."
+
+    def array_format(self, edit):
+        sels = self.view.sel()
+        for sel in sels:
+
+            region = self.view.line(sel)
+            lines = self.view.lines(region)
+
+            # Check it's there's more than one line
+            if len(lines) <= 1:
+                print "Wrong # of lines"
+                return()
+
+            # find if there's an array declaration in the selection,
+            #   and what line it's on
+            found_line = False
+            for line in lines:
+                if self.view.substr(line).find('array(') > 0:
+                    found_line = line
+
+            if found_line is False:
+                array_region = self.find_array_region(region)
+            else:
+                print self.view.substr(found_line)
+
+            full_array = self.view.substr(array_region)
+
+            print full_array
+
+    # Need to find where the array( bit is declared
+    def find_array_region(self, region):
+        beginning = region.begin()
+        end = region.end()
+
+        line_above = self.view.line(beginning-1)
+        line_below = self.view.line(end+1)
+
+        opening = False
+        while opening is False:
+            if self.view.substr(line_above).find('array(') != -1:
+                opening = line_above
+            elif line_above.begin() - 1 < 0:
+                print "out of bounds on top"
+                break
+            else:
+                line_above = self.view.line(line_above.begin() - 1)
+
+        closing = False
+        while closing is False:
+            if self.view.substr(line_below).find(');') != -1:
+                closing = line_below
+            elif line_below.end() + 1 > self.view.size():
+                print "out of bounds on bottom"
+                break
+            else:
+                line_below = self.view.line(line_below.end() + 1)
+
+        if opening is not False and closing is not False:
+            return sublime.Region(line_above.begin(), line_below.end())
+        else:
+            return False
 
     def control_statements(self, edit):
         sels = self.view.sel()
         for sel in sels:
-
-            print "ControlStatementSpacingCommand"
 
             lines_sel = self.view.line(sel)
             code = self.view.substr(lines_sel)
